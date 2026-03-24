76619cf9b3afbdb6f418b103866b926fb41953c4
iagoaran-code <iago.aran@deus.ai>
2026-03-24 10:44:21 +0100

    Update test_pokedex.py

--- test_pokedex.py
+++ test_pokedex.py
@@ -4,7 +4,6 @@ def test_api_returns_correct_id():
     bot = Pokedex()
     data = bot.get_pokemon_data("pikachu")
     
-    # This checks if the ID for Pikachu is actually 25
     assert data["id"] == 25
 
 def test_invalid_pokemon_returns_none():
